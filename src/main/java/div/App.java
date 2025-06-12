43d195cdcb2d613dbfc7a6e3fdbfbad541fab243
Anders Bekkevard <anders.bekkevard@gmail.com>
2025-06-12 14:24:11 +0200

    setup folder structure

--- src/main/java/div/App.java
+++ src/main/java/div/App.java
@@ -1,4 +1,4 @@
-package kladd;
+package div;
 
 public class App {
 	public static void main(String[] args) {
--- src/test/java/div/AppTest.java
+++ src/test/java/div/AppTest.java
@@ -1,4 +1,4 @@
-package kladd;
+package div;
 
 import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
