fc3bf716390053fd99ba65163bfef89a1cfaccf6
Torstein Fossnes <torsteinhf@gmail.com>
2025-06-17 09:26:06 +0200

    n

--- src/main/java/p01_intro_java/Operatorer.java
+++ src/main/java/p01_intro_java/Operatorer.java
@@ -5,36 +5,39 @@ public class Operatorer {
 	public static void main(String[] args) {
 
 		// Addere heltall
-		int antallEpler = 3;
-		int antallBananer = 5;
-		System.out.println(antallEpler + antallBananer);
+		int epler = 3;
+		int bananer = 5;
+		System.out.println(epler + bananer);
 
 		// Kan sette sammen regning med tekst for å gjøre utskrift mer lesbar
-		int antallFrukt = antallEpler + antallBananer;
-		System.out.println("Antall frukt: " + antallFrukt);
+		int frukt = epler + bananer;
+		System.out.println("Antall frukt: " + frukt);
 
 		// Bruker paranteser for å sikre rekkefølgen på utførelse
-		System.out.println("Antall frukt: " + antallEpler + antallBananer);
-		System.out.println("Antall frukt: " + (antallEpler + antallBananer));
+		System.out.println("Antall frukt: " + epler + bananer);
+		System.out.println("Antall frukt: " + (epler + bananer));
 
 		// Vi får så plutselig 4 appelsiner til
-		antallFrukt = antallFrukt + 1;
-		antallFrukt += 4;
-		antallFrukt -= 4;
-		antallFrukt++;
-		antallFrukt--;
+		frukt = frukt + 4;
+		frukt += 4;
+		frukt -= 4;
 		// antallFrukt *= 2;
 		// antallFrukt /= 2;
-		System.out.println("Antall epler, bananer og appelsiner: " + antallFrukt);
+		frukt += 1;
+		frukt++;
+		frukt--;
+		System.out.println("Antall epler, bananer og appelsiner: " + frukt);
 
 		// Datatypene som inngår påvirker datatypen til resultatet
 		int a = 5;
 		int b = 3;
-		System.out.println("5 / 3 = " + a / b);
+		System.out.println("5 + 3 = " + (a + b));
+
+		System.out.println("5 / 3 = " + (a / b));
 
 		double c = 3.0;
-		System.out.println("5 / 3.0 = " + a / c);
+		System.out.println("5 / 3.0 = " + (a / c));
 		// heltall / desimaltall = desimaltall
-		
+
 	}
 }
