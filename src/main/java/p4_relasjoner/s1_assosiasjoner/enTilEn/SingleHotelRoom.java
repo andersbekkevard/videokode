a3a15c09c554f0aaeac136aa0ff27564def19604
Anders Bekkevard <anders.bekkevard@gmail.com>
2025-07-07 11:12:34 +0200

    small

--- src/main/java/p4_relasjoner/s1_assosiasjoner/enTilEn/SingleHotelRoom.java
+++ src/main/java/p4_relasjoner/s1_assosiasjoner/enTilEn/SingleHotelRoom.java
@@ -2,36 +2,36 @@ package p4_relasjoner.s1_assosiasjoner.enTilEn;
 
 public class SingleHotelRoom {
 
-    private String roomName;
-    private Guest guest;
+	private String roomName;
+	private Guest guest;
 
-    public SingleHotelRoom(String roomName) {
-        if (roomName == null || roomName.isBlank()) {
-            throw new IllegalArgumentException();
-        }
-        this.roomName = roomName;
-    }
+	public SingleHotelRoom(String roomName) {
+		if (roomName == null || roomName.isBlank()) {
+			throw new IllegalArgumentException();
+		}
+		this.roomName = roomName;
+	}
 
-    // Over kan være inne fra start
+	// Over kan være inne fra start
 
-    public String getRoomName() {
-        return roomName;
-    }
+	public String getRoomName() {
+		return roomName;
+	}
 
-    public void setGuest(Guest student) {
-        this.guest = student;
-    }
+	public void setGuest(Guest guest) {
+		this.guest = guest;
+	}
 
-    public Guest getGuest() {
-        return guest;
-    }
+	public Guest getGuest() {
+		return guest;
+	}
 
-    @Override
-    public String toString() {
-        if (guest == null) {
-            return roomName + ": ingen beboer";
-        }
-        return roomName + ": " + guest.getName();
-    }
+	@Override
+	public String toString() {
+		if (guest == null) {
+			return roomName + ": ingen beboer";
+		}
+		return roomName + ": " + guest.getName();
+	}
 
 }
