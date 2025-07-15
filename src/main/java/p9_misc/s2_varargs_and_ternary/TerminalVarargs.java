b19448113469afa693fa093944f75b6a64a651b4
Anders Bekkevard <anders.bekkevard@gmail.com>
2025-07-15 14:48:34 +0200

    add stuff

--- src/main/java/p9_misc/s2_varargs_and_ternary/TerminalVarargs.java
+++ src/main/java/p9_misc/s2_varargs_and_ternary/TerminalVarargs.java
@@ -2,22 +2,21 @@ package p9_misc.s2_varargs_and_ternary;
 
 public class TerminalVarargs {
 
-    // Gjør dette etterpå
+	// Gjør dette etterpå
 
-    public static void main(String[] args) {
+	public static void main(String[] args) {
 
-        // Kanskje du lurer på hva som ligger i /target mappen
-        // Der ligger den kompilerte koden fra når vi kjører kode
-        // Vi kan også kompilere koden i terminalen
-    
-        // cd til s2_varargs
-        // javac Main.java
-        // java Main.java Hello World....
-        // Dette er egt en varargs som automatisk gjøres om 
-        for (String s : args) {
-            System.out.println(s);
-        }
-    }
+		// Kanskje du lurer på hva som ligger i /target mappen
+		// Der ligger den kompilerte koden fra når vi kjører kode
+		// Vi kan også kompilere koden i terminalen
 
+		// cd til s2_varargs
+		// javac Main.java
+		// java Main.java Hello World....
+		// Dette er egt en varargs som automatisk gjøres om
+		for (String s : args) {
+			System.out.println(s);
+		}
+	}
 
 }
