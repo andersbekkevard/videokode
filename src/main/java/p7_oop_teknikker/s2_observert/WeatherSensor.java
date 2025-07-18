3991a5c1684bef4cfe285133087d2d590e14095a
Anders Bekkevard <anders.bekkevard@gmail.com>
2025-07-18 09:55:49 +0200

    small change

--- src/main/java/p7_oop_teknikker/s2_observert/WeatherSensor.java
+++ src/main/java/p7_oop_teknikker/s2_observert/WeatherSensor.java
@@ -28,7 +28,6 @@ public class WeatherSensor {
 		listeners.forEach(l -> l.weatherUpdated(oldTemp, newTemp));
 	}
 
-
 	public static void main(String[] args) {
 
 		WeatherSensor sensor = new WeatherSensor();
